778f5336d12ac76168a997934faba2305bed03d4
prakashtaz0091 <code.ing2468@gmail.com>
2025-01-16 21:32:44 +0545

    shop

--- project/urls.py
+++ project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  #this is for the home page
     path('about/', include('about.urls')),
+    path('shop/', include('shop.urls')),
     
     
 ] 
